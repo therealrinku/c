5b4ccfcbcf59f489800e67cd134b5569da47feb4
Rinku Chaudhari <76877078+therealrinku@users.noreply.github.com>
2024-05-15 08:33:14 +0545

    feat: create calculator.c

---
+++ calculator.c
@@ -0,0 +1,50 @@
+/*
+  calculator app that performs addition,substraction, multiplication and division
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double calcuate(double a, double b, char op);
+
+int main(void){
+  double a;
+  char op;
+  double b;
+
+  printf("First Number: ");
+  scanf("%lf", &a);
+
+  printf("Operator (+, -, * or /): ");
+  scanf(" %c", &op);
+
+  printf("Second Number: ");
+  scanf("%lf", &b);
+
+  printf("Result is: %lf", calcuate(a, b, op));
+}
+
+double calcuate(double a , double b , char op){
+  double res;
+  
+  switch(op){
+    case '+':{
+      res = a + b;
+      break;
+    }
+    case '-':{
+      res = a - b;
+      break;
+    }
+    case '*':{
+      res = a * b;
+      break;
+    }
+    case '/':{
+      res = a / b;
+      break;
+    }
+  }
+
+  return res;
+}
