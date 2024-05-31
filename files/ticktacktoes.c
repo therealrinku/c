237c6b87e1b12e9d031ac2c88248dd9fb745c026
Rinku Chaudhari <76877078+therealrinku@users.noreply.github.com>
2024-05-31 12:26:58 +0545

    feat: rewrite ticktacktoes

--- files/ticktacktoes.c
+++ files/ticktacktoes.c
@@ -1,134 +1,215 @@
-// let's build a tick tack toes
+/*
+  tick tac toe -- rewrite on May 31 2024
+*/
 
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
+typedef struct{
+  int row;
+  int col;
+} RowColIndex;
+
+
+const char player = 'X';
+const char computer = 'C';
+// 'X' or 'C'; X for player and c for computer; ' ' if nobody has won yet, 'D' if draw
+char winner = ' ';
+char board[3][3] = {{ '0' , '0', '0' }, { '0' , '0', '0' }, { '0' , '0', '0' }};
+
+void printBoard(void);
 void movePlayer(void);
 void moveComputer(void);
-void checkWinner(void);
+RowColIndex getRowAndColIndex(int pos);
+// 0 if nobody won and 1 if someone won, -1 if draw
+int checkIfSomeoneWon(void);
 bool isBoardFilled(void);
-int checkIfSomeoneWonTheGame(void);
-void printBoard(void);
-char *getBoardIndex(int pos);
+void printWinner(void);
+void reset(void);
 
-struct Point {
-  int x;
-  int y;
-};
 
-static const char PLAYER = 'X';
-static const char COMPUTER = 'P';
+int main(void){
+  // s rand   
+  srand(time(NULL));
 
-char board[3][3] = {{'0', '0', '0'}, {'0', '0', '0'}, {'0', '0', '0'}};
+  printf("----WELCOME to ticktacktoes----\n");
 
-int main(void) {
-  printBoard();
-  movePlayer();
-  int winner = checkIfSomeoneWonTheGame();
-  if (winner != -1) {
-    return 0;
+  while(winner == ' '){
+    printBoard();
+
+    movePlayer();
+    int resp1 = checkIfSomeoneWon();
+    if(resp1 == 1 || resp1 == -1){
+      break;
+    }
+   
+    moveComputer();
+    int resp2 = checkIfSomeoneWon();
+    if(resp2 == 1 || resp2 == -1){
+      break;
+    }
   }
-  moveComputer();
-  int winnerA = checkIfSomeoneWonTheGame();
-  
-  if (winnerA != -1) {
-    return 0;
-  }
-  printBoard();
 
-  if (isBoardFilled()) {
-    printf("Game Over, It's a fucking tie.");
-  } else {
-    printf("Another Round\n");
+  printBoard();
+  printWinner();
+
+  char resp;
+  printf("Do you want to replay?, y or n:");
+  scanf("%s", &resp);
+
+  printf("\n");
+
+  if(strcmp(&resp, "y") == 0){
+    reset();
     main();
+  }else{
+    printf("Thanks for playing :))");
   }
-
+  
   return 0;
-};
+}
 
-void printBoard() {
-  for (int i = 0; i < 3; i++) {
-    printf("-- %c -- %c -- %c --\n", board[i][0], board[i][1], board[i][2]);
+void printBoard(void){
+  printf("\n");
+
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      printf("%c  ", board[i][j]);
+    }
+
     printf("\n");
   }
 }
 
-char *getBoardIndex(int pos) {
-  if (pos == 1) {
-    return "00";
-  } else if (pos == 2) {
-    return "01";
-  } else if (pos == 3) {
-    return "02";
-  } else if (pos == 4) {
-    return "10";
-  } else if (pos == 5) {
-    return "11";
-  } else if (pos == 6) {
-    return "12";
-  } else if (pos == 7) {
-    return "20";
-  } else if (pos == 8) {
-    return "21";
-  } else if (pos == 9) {
-    return "22";
-  } else {
-    return "Invalid";
-  }
-};
+void movePlayer(void){
+  printf("\n");
 
-void movePlayer() {
   int pos;
+  printf("Type a position you want to make a move (1-9):");
+  scanf("%i", &pos);
 
-  printf("Enter your next position(1-9):");
-  scanf("%d", &pos);
+  RowColIndex rowAndColIndex = getRowAndColIndex(pos);
 
-  char *posXY = getBoardIndex(pos);
-
-  if (strcmp(posXY, "Invalid") == 0) {
-    printf("Invalid Position, Retry Again!");
+  if(board[rowAndColIndex.row][rowAndColIndex.col] != '0'){
+    printf("Invalid position, it should be (1-9 and make sure it's not filled already.");
     movePlayer();
   }
-
-  int posX = posXY[0] - '0';
-  int posY = posXY[1] - '0';
-
-  if (board[posX][posY] != '0') {
-    printf("Invalid Position, Retry Again!");
-    movePlayer();
-  } else {
-    board[posX][posY] = PLAYER;
+  else {
+   board[rowAndColIndex.row][rowAndColIndex.col] =  player;
   }
 }
 
-void moveComputer() {
-  // Seed the random number generator
-   time_t t;
-   srand(time(&t));
+void moveComputer(void){
 
   int pos = rand() % 9 + 1;
 
-  char *posXY = getBoardIndex(pos);
-
-  int posX = posXY[0] - '0';
-  int posY = posXY[1] - '0';
-
-  if (board[posX][posY] != '0') {
+  RowColIndex rowAndColIndex = getRowAndColIndex(pos);
+  if(board[rowAndColIndex.row][rowAndColIndex.col] != '0'){
     moveComputer();
-  } else {
-    board[posX][posY] = COMPUTER;
+  }
+  else {
+   board[rowAndColIndex.row][rowAndColIndex.col] =  computer;
   }
 }
 
-bool isBoardFilled(void) {
+RowColIndex getRowAndColIndex(int pos){
+  RowColIndex point;
+
+  if (pos > 9 || pos < 0) {
+    point.row = -1;
+    point.col = -1;
+
+    return point;
+  }
+  
+
+  if(pos == 1){
+    point.row = 0;
+    point.col = 0;
+  }
+  else if(pos == 2){
+    point.row = 0;
+    point.col = 1;
+  }
+  else if(pos == 3){
+    point.row = 0;
+    point.col = 2;
+  }
+  else if(pos == 4){
+    point.row = 1;
+    point.col = 0;
+  }
+  else if(pos == 5){
+    point.row = 1;
+    point.col = 1;
+  }
+  else if(pos == 6){
+    point.row = 1;
+    point.col = 2;
+  }
+  else if(pos == 7){
+    point.row = 2;
+    point.col = 0;
+  }
+  else if(pos == 8){
+    point.row = 2;
+    point.col = 1;
+  }
+  else if(pos == 9){
+    point.row = 2;
+    point.col = 2;
+  }
+
+  return point;
+};
+
+int checkIfSomeoneWon(void){
+  // check row, check coolumn, check duiagonal
+  
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      //row
+      if(board[i][0]  != '0' && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+        winner = board[i][0];
+        return 1;
+        break;
+      }
+
+      //col
+      if(board[0][i]  != '0' && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+        winner = board[0][i];
+        return 1;
+        break;
+      }
+    }
+  }
+
+  //check diagonAL
+  if(board[0][0] != '0' && board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+    winner = board[0][0];
+    return 1;
+  }
+  else if (board[0][2] != '0' && board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+    winner = board[0][2];
+    return 1;
+  }
+
+  if(isBoardFilled() == true){
+    return -1;
+  }
+  
+  return 0;
+}
+
+bool isBoardFilled(void){
   int emptySpaces = 0;
 
-  for (int i = 0; i < 3; i++) {
-    for (int j = 0; j < 3; j++) {
-      if (board[i][j] == '0') {
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      if(board[i][j] == '0'){
         emptySpaces++;
       }
     }
@@ -137,37 +218,24 @@ bool isBoardFilled(void) {
   return emptySpaces == 0;
 }
 
-int checkIfSomeoneWonTheGame(void) {
-  char winner;
+void printWinner(void){
+  if(winner == player){
+    printf("YOu won!!");
+  }else if(winner == computer){
+    printf("You lost :(");
+  }else {
+    printf("Game was a tie");
+  }
 
-  // check rows and columns
-  for (int i = 0; i < 3; i++) {
-    // rows
-    if (board[i][0] != '0' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
-      winner = board[i][0];
-      break;
+  printf("\n");
+}
+
+void reset(void){
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      board[i][j] = '0';
     }
-    // cols
-    else if (board[0][i] != '0' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
-      winner = board[0][i];
-      break;
-    }
   }
 
-  // check diagonals
-  if (board[0][0] != '0' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-    winner = board[0][0];
-  } else if (board[0][2] != '0' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-    winner = board[0][2];
-  }
-
-  if (winner == PLAYER) {
-    printf("Game Over! You have won it bruh.");
-    return 0;
-  } else if (winner == COMPUTER) {
-    printf("Game Over! You're a loser.");
-    return 1;
-  }
-
-  return -1;
+  winner=' ';
 }
