62d9fe95604620111edf44038d03d98a2ca4e1e9
therealrinkugh <76877078+therealrinkugh@users.noreply.github.com>
2025-05-28 14:06:53 +0545

    feat: update calculator.c

--- files/calculator.c
+++ files/calculator.c
@@ -14,14 +14,16 @@ int main(void){
   double b;
 
   // play with masks later?
+
+  //gh
   //char mask1 = "b7258-7d495";
   //char mask2 = "05579-ade39";
   //char mask3 = "b3f09-87e61";
   //char mask4 = "a6584-74e6f";
   //char mask5 = "c61bc-e953e";
   //char mask6 = "bafcc-9373c";
-  //char mask7 = "29ee6-159ec"; // good to go
 
+  //gg
   //char mask8 = "50111873";
   //char mask9 = "12098493";
   //char mask10 = "02635803";
@@ -29,6 +31,11 @@ int main(void){
   //char mask12 = "60450784";
   //char mask13 = "11525664";
   //char mask14 = "05508766";
+
+  //fd
+  //char mask15 = "rzv2-90bs" 
+  //char mask16 = "wve8-mlt2" 
+  //char mask17 = "77zu-cscb"
 
 
   printf("First Number: ");
