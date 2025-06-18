07d3961881e3061182b4346b50409012ae824e8c
therealrinku <76877078+therealrinku@users.noreply.github.com>
2025-06-18 10:52:45 +0545

    fix: cleanup calculator.c

--- files/calculator.c
+++ files/calculator.c
@@ -13,33 +13,6 @@ int main(void){
   char op;
   double b;
 
-  // play with masks later?
-
-  //gh
-  //char mask1 = "b7258-7d495";
-  //char mask2 = "05579-ade39";
-  //char mask3 = "b3f09-87e61";
-  //char mask4 = "a6584-74e6f";
-  //char mask5 = "c61bc-e953e";
-  //char mask6 = "bafcc-9373c";
-
-  //gg
-  //char mask8 = "50111873";
-  //char mask9 = "12098493";
-  //char mask10 = "02635803";
-  //char mask11 = "58686070";
-  //char mask12 = "60450784";
-  //char mask13 = "11525664";
-  //char mask14 = "05508766";
-
-  //fd
-  //char mask15 = "rzv2-90bs" 
-  //char mask16 = "wve8-mlt2" 
-  //char mask17 = "77zu-cscb"
-
-  //ap
-  //char mask18 = "2921-3381-3512"
-
   printf("First Number: ");
   scanf("%lf", &a);
 
